cb443733d18a681f3531a2e0f48fc7512c0f8943
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-31 20:40:12 -0300

    Implement PatientDetails data fetching logic

--- src/pages/PatientDetails.tsx
+++ src/pages/PatientDetails.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useAsync, useIsMounted } from "@henriqueinonhe/react-hooks";
+import React, { useState } from "react";
+import { useParams } from "react-router";
 import { PageLayout } from "../components/PageLayout";
+import { AppointmentsService, RawAppointment } from "../services/AppointmentsService";
+import { Patient, PatientsService } from "../services/PatientsService";
 
 export function PatientDetails() : JSX.Element {
+  const { id } = useParams<{ id : string }>();
+  const patientId = parseInt(id);
+  const [patient, setPatient] = useState<Patient>();
+  const [appointments, setAppointments] = useState<Array<RawAppointment>>([]);
+  const [dataIsLoading, setDataIsLoading] = useState(true);
+  const isMounted = useIsMounted();
+
+  useAsync(isMounted, async () => {
+    return await Promise.all([
+      AppointmentsService.fetchRawAppointments(),
+      PatientsService.fetchSinglePatient(patientId)
+    ]);
+  }, ([fetchedAppointments, fetchedPatient]) => {
+    setAppointments(fetchedAppointments.filter(appointment => 
+      appointment.patientId === fetchedPatient.id));
+    setPatient(fetchedPatient);
+  }, [], setDataIsLoading);
+
   return (
     <PageLayout>
       
