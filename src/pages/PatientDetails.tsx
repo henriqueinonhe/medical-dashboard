f3a9ec683bff3d999a313532f14ac279fd2bfc21
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-31 09:13:48 -0300

    Implement PatientDetails page skeleton

---
+++ src/pages/PatientDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PageLayout } from "../components/PageLayout";
+
+export function PatientDetails() : JSX.Element {
+  return (
+    <PageLayout>
+      
+    </PageLayout>
+  );
+}
+
+export default PatientDetails;
