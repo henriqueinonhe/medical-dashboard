e7a980e17355ca33d6f56adccba67ca0ed826a91
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-31 09:13:35 -0300

    Implement DoctorDashboard page skeleton

---
+++ src/pages/DoctorDashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PageLayout } from "../components/PageLayout";
+
+export function DoctorDashboard() : JSX.Element {
+  return (
+    <PageLayout>
+      
+    </PageLayout>
+  );
+}
+
+export default DoctorDashboard;
