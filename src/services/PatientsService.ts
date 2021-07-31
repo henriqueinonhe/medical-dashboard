92217497193877c6fcc58c7689a77d91ed219f3d
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-31 18:20:30 -0300

    Implement fetchPatients

--- src/services/PatientsService.ts
+++ src/services/PatientsService.ts
@@ -1,3 +1,5 @@
+import { apiClient } from "../helpers/apiHelper";
+
 export type InsurancePlan = "Regional" | "NationalBasic" |
                             "NationalPremium" | "International" |
                             "Diamond";
@@ -9,4 +11,25 @@ export type Patient = {
   healthSystemId : string;
   birthday : string;
   insurancePlan : InsurancePlan;
+}
+
+export class PatientsService {
+  public static displayableInsurancePlan : {
+    [Key in InsurancePlan] : string;
+  } = {
+    Regional: "Regional",
+    NationalBasic: "National Basic",
+    NationalPremium: "National Premium",
+    International: "International",
+    Diamond: "Diamond"
+  };
+
+  public static async fetchPatients() : Promise<Array<Patient>> {
+    const response = await apiClient.request({
+      url: "/patients",
+      method: "GET"
+    });
+
+    return response.data;
+  }
 }
