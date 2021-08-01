96d3a60be27b97d548de49f4e8a917dc480bf1ab
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-01 09:52:56 -0300

    Update PageLayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const Container = styled.div`
   min-height: 100%;
   background-color: #e8ebed;
+  padding-bottom: 60px;
 `;
 
 export interface PageLayoutProps {
