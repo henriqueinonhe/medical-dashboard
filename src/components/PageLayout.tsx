b410f563cd0ff5e967aeecac0fd6bd872e857988
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-04 23:38:35 -0300

    Update doctor dashboard navbar

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { Header } from "./Header";
 
 const Container = styled.div`
   min-height: 100%;
   background-color: #e8ebed;
   padding-bottom: 60px;
-`;
-
-const ChildrenContainer = styled.main`
-  margin-top: 40px;
 `;
 
 export interface PageLayoutProps {
@@ -23,11 +18,7 @@ export function PageLayout(props : PageLayoutProps) : JSX.Element {
 
   return (
     <Container>
-      <Header />
-
-      <ChildrenContainer>
-        {children}
-      </ChildrenContainer>
+      {children}
     </Container>
   );
 }
--- src/pages/DoctorDashboard.tsx
+++ src/pages/DoctorDashboard.tsx
@@ -7,13 +7,44 @@ import { Appointment, AppointmentsService } from "../services/AppointmentsServic
 import { Patient, PatientsService } from "../services/PatientsService";
 import { useAsync, useIsMounted } from "@henriqueinonhe/react-hooks";
 import { PatientsList } from "../components/PatientsList";
-import { Route, Switch } from "react-router";
+import { Route, Switch, useLocation } from "react-router";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const NavLinkList = styled.div`
+  display: flex;
+  background-color: white;
+  min-width: 700px;
+  max-width: 1000px;
+  width: 70vw;
+  margin: auto;
+  padding: 24px;
+  background-color: white;
+  border-radius: 3px;
+`;
+
+interface NavLinkProps {
+  active : boolean;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const NavLink = styled(({active, ...props}) => <Link {...props} />)<NavLinkProps>`
+  margin-left: 20px;
+  padding: 12px;
+  border-radius: 4px;
+  text-decoration: none;
+  font-family: 18px;
+
+  background-color: ${props => props.active ? "#4a7dff" : "transparent"};
+  color: ${props => props.active ? "white" : "initial"};
+`;
 
 export function DoctorDashboard() : JSX.Element {
   const [patients, setPatients] = useState<Array<Patient>>([]);
   const [appointments, setAppointments] = useState<Array<Appointment>>([]);
   const [dataIsLoading, setDataIsLoading] = useState(true);
   const isMounted = useIsMounted();
+  const location = useLocation();
   
   useAsync(isMounted, async () => {
     return await Promise.all([
@@ -30,6 +61,29 @@ export function DoctorDashboard() : JSX.Element {
 
   return (
     <PageLayout>
+      <NavLinkList>
+        <NavLink
+          to="/dashboard/calendar"
+          active={location.pathname === "/dashboard/calendar"}
+        >
+          Calendar
+        </NavLink>
+
+        <NavLink
+          to="/dashboard/history"
+          active={location.pathname === "/dashboard/history"}
+        >
+          History
+        </NavLink>
+
+        <NavLink
+          to="/dashboard/patients"
+          active={location.pathname === "/dashboard/patients"}
+        >
+          Patients
+        </NavLink>
+      </NavLinkList>
+
       {
         dataIsLoading ?
           <>Loading...</> :
